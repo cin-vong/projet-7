ec092c9c41262a69d64a6d36f27f0c809f6faf58
cin-vong <61393238+cin-vong@users.noreply.github.com>
2021-01-17 12:35:11 +0100

    commit-01

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <Login msg="Bienvenue au forum !"/>
+  </div>
+</template>
+
+<script>
+import Login from '@/components/Login.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Login
+  }
+}
+</script>
